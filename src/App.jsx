d77e9724241ddbcfa8b56f7cb22543d0e95c8f87
Yie <pumasami7@gmail.col>
2022-11-23 13:40:25 +0100

    button

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.scss";
+const App = () => {
+  return (
+    <div class="btn-group6">
+      <a class="btn6">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Hover Button
+      </a>
+    </div>
+  );
+};
+
+export default App;
